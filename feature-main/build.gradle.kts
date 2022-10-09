37cb2d54252b7bc3b268a24f30655cb8c96212ed
isaac <realjoong@gmail.com>
2022-10-09 10:05:10 +0900

    add main screen, remove namespace from common gradle

---
+++ feature-main/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.farmer.feature_main"
+}
+
+dependencies {
+
+    // base
+    implementation(libs.bundles.compose)
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle)
+    implementation(libs.lifecycle.viewmodel)
+
+    // hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.android.junit)
+}
---
+++ feature-main/src/main/kotlin/com/farmer/feature_main/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.farmer.feature_main
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Text("Hello Main Screen!")
+}
