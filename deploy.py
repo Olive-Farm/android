e5bf9e8e3d0bd7e3efdd01e30e8afce1f0efb472
이원중 Isaac Lee <57510192+WonJoongLee@users.noreply.github.com>
2022-10-15 12:36:44 +0900

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -7,7 +7,7 @@ def main():
     androidSlackBotToken = os.environ['ANDROID_SLACK_BOT_TOKEN']
     client = WebClient(token=androidSlackBotToken)
     response = client.files_upload_v2(
-        file="@app/build/outputs/apk/debug/app-debug.apk",
+        file="app/build/outputs/apk/debug/app-debug.apk", 
         title="Wonjoong test",
         channel="C045WAKTQ6Q",
         initial_comment="Wonjoong is testing right now",
