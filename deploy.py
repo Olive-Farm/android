70f6d4959e47c238ed7925f83b993cccc9b92be4
isaac <realjoong@gmail.com>
2022-10-15 11:04:09 +0900

    add deploy.py

---
+++ deploy.py
@@ -0,0 +1,19 @@
+import os
+
+from slack_sdk import WebClient
+
+
+def main():
+    androidSlackBotToken = os.environ("ANDROID_SLACK_BOT_TOKEN")
+    client = WebClient(token=androidSlackBotToken)
+    response = client.files_upload_v2(
+        file="@app/build/outputs/apk/debug/app-debug.apk",
+        title="Wonjoong test",
+        channel="C045WAKTQ6Q",
+        initial_comment="Wonjoong is testing right now",
+    )
+    response.get("file")
+
+
+if __name__ == "__main__":
+    main()
