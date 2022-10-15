94f16f95f997a48e2e30af8c759ec7f244858c46
이원중 Isaac Lee <57510192+WonJoongLee@users.noreply.github.com>
2022-10-15 12:30:26 +0900

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -4,7 +4,7 @@ from slack_sdk import WebClient
 
 
 def main():
-    androidSlackBotToken = os.environ("ANDROID_SLACK_BOT_TOKEN")
+    androidSlackBotToken = os.environ('ANDROID_SLACK_BOT_TOKEN')
     client = WebClient(token=androidSlackBotToken)
     response = client.files_upload_v2(
         file="@app/build/outputs/apk/debug/app-debug.apk",
