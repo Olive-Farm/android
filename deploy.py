1b85188586345e988e36a83b8e8805bc3dc0fdf1
이원중 Isaac Lee <57510192+WonJoongLee@users.noreply.github.com>
2022-10-15 13:17:09 +0900

    add thread of slack

--- deploy.py
+++ deploy.py
@@ -5,14 +5,14 @@ from slack_sdk import WebClient
 
 def main():
     androidSlackBotToken = os.environ['ANDROID_SLACK_BOT_TOKEN']
-    branchName = os.environ['CURRENT_BRANCH']
     androidSlackChannelKey = "C045WAKTQ6Q"
     client = WebClient(token=androidSlackBotToken)
     response = client.files_upload_v2(
-        file="app/build/outputs/apk/debug/app-debug.apk", 
+        file="app/build/outputs/apk/debug/app-debug.apk",
         title="Android apk build",
         channel=androidSlackChannelKey,
         initial_comment="Apk was successfully build! :white_check_mark:",
+        thread_ts="1665807214.774699"
     )
     response.get("file")
 
