5888d39cbb2e12a97a2135fef6fed7241722185f
isaac <realjoong@gmail.com>
2022-11-09 23:33:23 +0900

    add startup benchmark

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -21,7 +21,6 @@ android {
     defaultConfig {
         minSdk = Configuration.minSdk
         targetSdk = Configuration.targetSdk
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -30,7 +29,7 @@ android {
         // release build (for example, with minification on). It"s signed with a debug key
         // for easy local/CI testing.
         create("benchmark") {
-            isDebuggable = true
+            isDebuggable = false
             signingConfig = getByName("debug").signingConfig
             matchingFallbacks += listOf("release")
         }
--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -3,7 +3,7 @@ package com.farmer.olive
 object Configuration {
     const val compileSdk = 33
     const val targetSdk = 33
-    const val minSdk = 21
+    const val minSdk = 23
     private const val majorVersion = 0
     private const val minorVersion = 0
     private const val patchVersion = 1
