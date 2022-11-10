1986183002ad065e6124695d54dfbdbdfaee4d6f
isaac <realjoong@gmail.com>
2022-11-10 22:49:30 +0900

    change cashbook icon and statistics icon

--- app/src/main/kotlin/com/farmer/olive/ui/OliveBottomNavigation.kt
+++ app/src/main/kotlin/com/farmer/olive/ui/OliveBottomNavigation.kt
@@ -4,8 +4,6 @@ import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
@@ -24,7 +22,7 @@ fun OliveBottomNavigation(
         screenList.forEach { screen ->
             BottomNavigationItem(
                 icon = {
-                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                    Icon(screen.icon, contentDescription = null)
                 },
                 label = {
                     Text(screen.screenName)
--- app/src/main/kotlin/com/farmer/olive/ui/OliveScreens.kt
+++ app/src/main/kotlin/com/farmer/olive/ui/OliveScreens.kt
@@ -1,13 +1,27 @@
 package com.farmer.olive.ui
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.graphics.vector.ImageVector
+
 // todo screen name should be changed to string id
 sealed class OliveScreens(
     val route: String,
-    val screenName: String = ""
+    val screenName: String = "",
+    val icon: ImageVector
 ) {
     // cash book screen
-    object CashBook : OliveScreens("cash_book", "Cashbook")
+    object CashBook : OliveScreens(
+        route = "cash_book",
+        screenName = "Cashbook",
+        icon = Icons.Filled.List
+    )
 
     // statistics screen
-    object Statistics : OliveScreens("statistics", "Statistics")
+    object Statistics : OliveScreens(
+        route = "statistics",
+        screenName = "Statistics",
+        icon = Icons.Filled.ShoppingCart
+    )
 }
