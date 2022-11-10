4f9de64763a6826e37c22e132ad4ad834636e77d
isaac <realjoong@gmail.com>
2022-11-10 23:07:53 +0900

    add working on it sign at statistics screen

--- feature-statistics/src/main/kotlin/com/farmer/feature_statistics/StatisticsScreen.kt
+++ feature-statistics/src/main/kotlin/com/farmer/feature_statistics/StatisticsScreen.kt
@@ -1,9 +1,42 @@
 package com.farmer.feature_statistics
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun StatisticsScreen() {
-    Text(text = "Hello this is statistics screen!")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "⚠️ Under Construction ⚠️",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+
+//        Image(
+//            modifier = Modifier.size(140.dp),
+//            painter = painterResource(R.drawable.img_working_on),
+//            contentDescription = null
+//        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "Team Olive is currently working on it.",
+            fontSize = 14.sp
+        )
+    }
 }
