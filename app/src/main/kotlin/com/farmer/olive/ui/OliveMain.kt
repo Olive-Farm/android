282445f65925fe0ba35cddeb12d636b04fd74d48
isaac <realjoong@gmail.com>
2022-12-06 05:42:04 +0900

    change bottom navigation design with fab

--- app/src/main/kotlin/com/farmer/olive/ui/OliveMain.kt
+++ app/src/main/kotlin/com/farmer/olive/ui/OliveMain.kt
@@ -1,11 +1,20 @@
 package com.farmer.olive.ui
 
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.compose.rememberNavController
 import com.farmer.olive.navigation.OliveNavHost
+import com.farmer.olive.ui.theme.Green500
 import com.farmer.olive.ui.theme.OliveTheme
 
 @Composable
@@ -16,9 +25,24 @@ fun OliveMain(
     OliveTheme {
         val navHostController = rememberNavController()
         Scaffold(
+            floatingActionButtonPosition = FabPosition.Center,
             bottomBar = {
-                OliveBottomNavigation(navHostController, viewModel.oliveScreens)
-            }
+                BottomAppBar(
+                    cutoutShape = CircleShape,
+                    backgroundColor = Color.White
+                ) {
+                    OliveBottomNavigation(navHostController, viewModel.oliveScreens)
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    backgroundColor = Green500,
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(Icons.Default.Add, contentDescription = null)
+                }
+            },
+            isFloatingActionButtonDocked = true
         ) {
             OliveNavHost(navHostController = navHostController, composeNavigator = composeNavigator)
         }
