2bd2a945cb1db2ef0a53df0426189d1fa86ba630
isaac <realjoong@gmail.com>
2023-03-25 18:15:46 +0900

    필요 없는 코드 제거

--- app/src/main/kotlin/com/farmer/olive/navigation/OliveNavHost.kt
+++ app/src/main/kotlin/com/farmer/olive/navigation/OliveNavHost.kt
@@ -1,6 +1,5 @@
 package com.farmer.olive.navigation
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -20,12 +19,6 @@ fun OliveNavHost(
 ) {
     NavHost(navController = navHostController, startDestination = OliveScreens.CashBook.route) {
         composable(route = "cash_book") {
-            Log.e("@@@navBackStackEntry", "it : ${it}")
-            Log.e("@@@navBackStackEntry", "it : ${it.arguments}")
-            Log.e("@@@navBackStackEntry", "it : ${it.destination}")
-            Log.e("@@@navBackStackEntry", "it : ${it.id}")
-            Log.e("@@@navBackStackEntry", "it : ${it.maxLifecycle}")
-            Log.e("@@@navBackStackEntry", "it : ${it.savedStateHandle}")
             CashbookScreen(calendarViewModel)
         }
         composable(route = "statistics") {
