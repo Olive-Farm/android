681eaddcaaa578fb00504f5995da1e04c13d0940
isaac <realjoong@gmail.com>
2023-04-26 14:04:17 +0900

    기본적인 데이터바인딩 연결 로직 추가

--- feature-settings/src/main/java/com/farmer/feature_settings/SettingsActivity.kt
+++ feature-settings/src/main/java/com/farmer/feature_settings/SettingsActivity.kt
@@ -1,7 +1,15 @@
 package com.farmer.feature_settings
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.databinding.DataBindingUtil
+import com.farmer.feature_settings.databinding.ActivitySettingsBinding
 
 class SettingsActivity: ComponentActivity() {
-
+    private lateinit var binding : ActivitySettingsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
+        binding.tvTemp.text = "이런 식으로 연결"
+    }
 }
