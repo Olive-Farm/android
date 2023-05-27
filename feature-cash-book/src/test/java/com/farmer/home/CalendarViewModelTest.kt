12d080b3e4b6937189dd01b5cf84f1b162d5abdf
isaac <realjoong@gmail.com>
2023-05-27 20:24:38 +0900

    test 깨지는 거 수정

--- feature-cash-book/src/test/java/com/farmer/home/CalendarViewModelTest.kt
+++ feature-cash-book/src/test/java/com/farmer/home/CalendarViewModelTest.kt
@@ -2,9 +2,11 @@ package com.farmer.home
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.testing.TestLifecycleOwner
+import com.farmer.data.repository.OliveRepository
 import com.farmer.home.data.CashBookRepository
 import com.farmer.home.ui.states.CalendarUiState
 import com.farmer.home.ui.states.CalendarViewModel
+import com.farmer.network.BaseResponse
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
@@ -22,6 +24,7 @@ class CalendarViewModelTest {
 
     private lateinit var testLifecycleOwner: LifecycleOwner
     private val cashBookRepository = mockk<CashBookRepository>()
+    private val oliveRepository = mockk<OliveRepository>()
     private val dispatcher = StandardTestDispatcher()
 
     @Before
@@ -39,11 +42,11 @@ class CalendarViewModelTest {
     fun `값을_입력하면_데이터_추가_후_다이얼로그_끄기`() {
         runTest {
             // given
-            val viewModel = CalendarViewModel(cashBookRepository)
+            val viewModel = CalendarViewModel(cashBookRepository, oliveRepository)
             val time = "2022-12-15"
             val name = "버거킹"
             val amount = "-50000"
-            coEvery { cashBookRepository.getAllUserData() } returns emptyList()
+            coEvery { cashBookRepository.getAllUserData() } returns BaseResponse.Success(emptyList())
 
             // when
             viewModel.sendInputCashDataAndDismiss(time, name, amount)
@@ -57,10 +60,10 @@ class CalendarViewModelTest {
     fun `화면_초기화시_state는_loading_state`() {
         runTest {
             // given
-            coEvery { cashBookRepository.getAllUserData() } returns emptyList()
+            coEvery { cashBookRepository.getAllUserData() } returns BaseResponse.Success(emptyList())
 
             // when
-            val viewModel = CalendarViewModel(cashBookRepository)
+            val viewModel = CalendarViewModel(cashBookRepository, oliveRepository)
 
             // then
             assert(viewModel.uiState.value is CalendarUiState.Loading)
