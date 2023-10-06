4a56ba5058dca6f78e34b969682bd8db63b930e1
isaac <realjoong@gmail.com>
2023-10-07 08:11:25 +0900

    swipe delete ui 구현

--- feature-cash-book/src/main/kotlin/com/farmer/home/ui/detail/DetailDialog.kt
+++ feature-cash-book/src/main/kotlin/com/farmer/home/ui/detail/DetailDialog.kt
@@ -1,24 +1,31 @@
 package com.farmer.home.ui.detail
 
 import android.util.Log
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissState
+import androidx.compose.material.DismissValue
 import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,8 +33,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -36,18 +43,15 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.feature_post.PostViewModel
 import com.farmer.data.DateInfo
 import com.farmer.data.History
-import com.farmer.data.HistoryList
-import com.farmer.feature_post.R
 import com.farmer.home.ui.BlueAlpha200
 import com.farmer.home.ui.RedAlpha200
 import com.farmer.home.ui.postdialog.PostDialogViewModel
 import com.farmer.home.ui.states.CalendarViewModel
+
 // 여기서 ui 수정?
 
 @Composable
@@ -55,7 +59,7 @@ fun DetailDialog(
     dateInfo: DateInfo?,
     isDialogEditMode: Boolean,
     viewModel: CalendarViewModel = hiltViewModel(),
-    postViewModel : PostDialogViewModel = hiltViewModel()
+    postViewModel: PostDialogViewModel = hiltViewModel()
 ) {
 
     Log.e("@@@dateInfo", ": $dateInfo")
@@ -74,7 +78,8 @@ fun DetailDialog(
 
         if (dateInfo?.history?.spendList?.earnList.isNullOrEmpty() && dateInfo?.history?.spendList?.spendList.isNullOrEmpty()) {
             Column(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
                     .padding(bottom = 20.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
@@ -86,18 +91,24 @@ fun DetailDialog(
                 )
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Text(buildAnnotatedString {
-                    withStyle(style = SpanStyle(fontWeight = FontWeight.SemiBold, color = Color(0xFF355A1E))) {
-                        append("OliveBook")
-                    }
-                    withStyle(style = SpanStyle(color = Color.DarkGray)) {
-                        append("을 이용하여\n 편리하게 가계부를 관리해보세요")
-                    }
-                }, fontSize = 14.sp,
-                    textAlign = TextAlign.Center)
+                Text(
+                    buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.SemiBold,
+                                color = Color(0xFF355A1E)
+                            )
+                        ) {
+                            append("OliveBook")
+                        }
+                        withStyle(style = SpanStyle(color = Color.DarkGray)) {
+                            append("을 이용하여\n 편리하게 가계부를 관리해보세요")
+                        }
+                    }, fontSize = 14.sp,
+                    textAlign = TextAlign.Center
+                )
             }
-        }
-        else {
+        } else {
             LazyColumn {
                 itemsIndexed(
                     dateInfo?.history?.spendList?.earnList ?: emptyList()
@@ -142,42 +153,7 @@ fun DetailDialog(
                 itemsIndexed(
                     dateInfo?.history?.spendList?.spendList ?: emptyList()
                 ) { index, spendData ->
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        val titleVisibility = if (index == 0) 1f else 0f
-                        Text(
-                            text = "지출",
-                            modifier = Modifier.alpha(titleVisibility),
-                            fontWeight = FontWeight.Bold
-                        )
-                        Spacer(modifier = Modifier.width(15.dp))
-                        Text(text = spendData.item)
-                        Spacer(modifier = Modifier.weight(1f))
-                        Text(
-                            text = String.format("%,d", spendData.price.toString().toLong()),
-                            //color = RedAlpha200
-                            fontWeight = FontWeight.SemiBold
-                        )
-
-                        Text(text = " ￦") // 나중에 이거 삭제하기 지금 테스트
-
-                        if (isDialogEditMode) {
-                            IconButton(
-                                modifier = Modifier
-                                    .padding(0.dp)
-                                    .size(16.dp),
-                                onClick = { /* todo */ }
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.Delete,
-                                    contentDescription = null,
-                                    tint = Color.Gray,
-                                )
-
-                            }
-                        }
-                    }
+                    TransactItem(index, spendData, isDialogEditMode)
 
                     Spacer(modifier = Modifier.height(4.dp))
                 }
@@ -190,7 +166,8 @@ fun DetailDialog(
 
             Spacer(modifier = Modifier.height(12.dp))
 
-            val sumOfIncome = (dateInfo?.history?.spendList?.earnList ?: emptyList()).sumOf { it.price }
+            val sumOfIncome =
+                (dateInfo?.history?.spendList?.earnList ?: emptyList()).sumOf { it.price }
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -210,7 +187,8 @@ fun DetailDialog(
 
             Spacer(modifier = Modifier.height(6.dp))
 
-            val sumOfSpend = (dateInfo?.history?.spendList?.spendList ?: emptyList()).sumOf { it.price }
+            val sumOfSpend =
+                (dateInfo?.history?.spendList?.spendList ?: emptyList()).sumOf { it.price }
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -230,8 +208,10 @@ fun DetailDialog(
 
             Spacer(modifier = Modifier.weight(1f))
 
-            Row(modifier = Modifier.padding(all = 7.dp),
-                verticalAlignment = Alignment.CenterVertically) {
+            Row(
+                modifier = Modifier.padding(all = 7.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
 
                 Spacer(modifier = Modifier.weight(1f))
 
@@ -289,3 +269,117 @@ fun DetailDialog(
 }
 
 
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun TransactItem(
+    index: Int,
+    spendData: History.Transact.TransactData,
+    isDialogEditMode: Boolean
+) {
+    val roundedCornerShape = RoundedCornerShape(10.dp)
+    val dismissState = rememberDismissState(confirmStateChange = { dismissValue ->
+        when (dismissValue) {
+            DismissValue.Default -> { // dismissThresholds 만족 안한 상태
+                false
+            }
+
+            DismissValue.DismissedToEnd -> { // -> 방향 스와이프 (수정)
+                // 지원 x
+                false
+            }
+
+            DismissValue.DismissedToStart -> { // <- 방향 스와이프 (삭제)
+
+                true
+            }
+        }
+    })
+
+    SwipeToDismiss(
+        modifier = Modifier.clip(roundedCornerShape),
+        state = dismissState,
+        dismissThresholds = { FractionalThreshold(0.25f) },
+        background = { // 기존에 보일 항목
+            val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
+            val color by animateColorAsState(
+                when (dismissState.targetValue) {
+                    DismissValue.Default -> backgroundColor.copy(alpha = 0.5f) // dismissThresholds 만족 안한 상태
+                    DismissValue.DismissedToEnd -> Color.Red.copy(alpha = 0.5f) // -> 방향 스와이프 (수정)
+                    DismissValue.DismissedToStart -> Color.Red.copy(alpha = 0.5f) // <- 방향 스와이프 (삭제)
+                }, label = ""
+            )
+            val alignment = when (direction) {
+                DismissDirection.EndToStart -> Alignment.CenterEnd
+                DismissDirection.StartToEnd -> Alignment.CenterStart
+            }
+            val icon = when (dismissState.targetValue) {
+                DismissValue.Default -> null
+                DismissValue.DismissedToEnd -> Icons.Default.Delete
+                DismissValue.DismissedToStart -> Icons.Default.Delete
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color)
+                    .padding(horizontal = 30.dp),
+                contentAlignment = alignment
+            ) {
+                if (icon != null) {
+                    Icon(imageVector = icon, contentDescription = null)
+                }
+            }
+
+        },
+        dismissContent = { // 지워지면 보일 항목
+            TransactContent(index, spendData, isDialogEditMode, dismissState)
+        })
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun TransactContent(
+    index: Int,
+    spendData: History.Transact.TransactData,
+    isDialogEditMode: Boolean,
+    dismissState: DismissState
+) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.background(Color.White)
+    ) {
+        val titleVisibility = if (index == 0) 1f else 0f
+
+        Text(
+            text = "지출",
+            modifier = Modifier.alpha(titleVisibility),
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.width(15.dp))
+        Text(text = spendData.item)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = String.format("%,d", spendData.price.toString().toLong()),
+            //color = RedAlpha200
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Text(text = " ￦") // 나중에 이거 삭제하기 지금 테스트
+
+        if (isDialogEditMode) {
+            IconButton(
+                modifier = Modifier
+                    .padding(0.dp)
+                    .size(16.dp),
+                onClick = { /* todo */ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                    tint = Color.Gray,
+                )
+
+            }
+        }
+    }
+}
