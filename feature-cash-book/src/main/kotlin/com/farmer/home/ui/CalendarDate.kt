53620eeee59ce9899087862a45acaffeedce1d1c
gyub99 <kimgubin@naver.com>
2023-10-18 21:55:29 +0900

    오늘 날짜 표시

--- feature-cash-book/src/main/kotlin/com/farmer/home/ui/CalendarDate.kt
+++ feature-cash-book/src/main/kotlin/com/farmer/home/ui/CalendarDate.kt
@@ -1,5 +1,6 @@
 package com.farmer.home.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -15,14 +16,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.datetime.DayOfWeek
 import java.text.DecimalFormat
+import java.time.LocalDate
 
 @Composable
 fun CalendarDate(
@@ -30,9 +34,17 @@ fun CalendarDate(
     income: Int,
     spend: Int,
     dayOfWeek: kotlinx.datetime.DayOfWeek?,
+    year: String,
+    month: String,
     onClick: () -> Unit
 ) {
     val numFormat = DecimalFormat("#,###")
+    val todayDate = LocalDate.now().toString().split("-")
+    val border = if (year == todayDate[0] && month == todayDate[1] && date == todayDate[2]) {
+        BorderStroke(1.dp, Color.Black) // 조건을 만족할 때의 BorderStroke
+    } else {
+        BorderStroke(0.5.dp, Color(0x80C2C2C2)) // 조건을 만족하지 않을 때의 BorderStroke
+    }
 
     Column(         //일자 한 칸
         modifier = Modifier
@@ -43,14 +55,13 @@ fun CalendarDate(
             }
             .padding(1.dp)
             .background(color =
-            if (dayOfWeek == java.time.DayOfWeek.SATURDAY || dayOfWeek == java.time.DayOfWeek.SUNDAY) Color(0xFFF6F6F6)
+            if (dayOfWeek == java.time.DayOfWeek.SATURDAY || dayOfWeek == java.time.DayOfWeek.SUNDAY)
+                Color(0xFFF6F6F6)
             else Color.White,
                 shape = RoundedCornerShape(size = 10.dp)
             )
-
-            .border(width = 0.5.dp, color = Color(0x80C2C2C2), shape = RoundedCornerShape(size = 10.dp))
-
-            .size(90.dp)
+            .border(border, shape = RoundedCornerShape(size = 10.dp))
+            .size(70.dp)
             .padding(5.dp)
     ) {
         Text(text = date, color =
@@ -70,11 +81,12 @@ fun CalendarDate(
                 Text(
                     modifier = Modifier.padding(vertical = 2.dp, horizontal = 4.dp),
                     text = numFormat.format(spend).toString(),
-                    fontSize = 10.sp,
+                    fontSize = 8.sp,
                     color = Color.White,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
-                    fontWeight = FontWeight.SemiBold
+                    fontWeight = FontWeight.SemiBold,
+
                 )
             }
         }
@@ -91,7 +103,7 @@ fun CalendarDate(
                 Text(
                     modifier = Modifier.padding(vertical = 2.dp, horizontal = 4.dp),
                     text = numFormat.format(income).toString(),
-                    fontSize = 10.sp,
+                    fontSize = 8.sp,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
                     color = Color.White,
@@ -113,6 +125,8 @@ fun CalendarDatePreview() {
         income = 500000,
         spend = 90000,
         dayOfWeek = java.time.DayOfWeek.SATURDAY,
+        year = "2023",
+        month = "8",
         onClick = {}
     )
 }
