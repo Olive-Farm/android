ab3080e3696bcdb10dd3383f5ac1a3d456fbe24e
gyub99 <kimgubin@naver.com>
2023-10-27 16:33:09 +0900

    전체 UI 성공

--- feature-settings/src/main/java/com/farmer/feature_settings/EditCategory.kt
+++ feature-settings/src/main/java/com/farmer/feature_settings/EditCategory.kt
@@ -1,8 +1,13 @@
 package com.farmer.feature_settings
 
+import android.content.Context
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -12,10 +17,13 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -23,6 +31,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
+import kotlin.math.roundToInt
 
 //@OptIn(ExperimentalMaterialApi::class, ExperimentalPermissionApi::class)
 @Composable
@@ -32,7 +41,11 @@ fun EditCategory(
 ){
     val uiState = viewModel.uiState.collectAsState()
 
+    val context = LocalContext.current
+
     val categoryList by selectCategoryList(viewModel).collectAsState(initial = emptyList())
+
+
 
     Column(modifier = Modifier.padding(horizontal = 20.dp, vertical = 20.dp)) {
         Row(
@@ -83,40 +96,15 @@ fun EditCategory(
                     }
                 }
             }
-            categoryList?.forEach { category ->
-                Row(modifier = Modifier.fillMaxWidth()
-                ){
-                    Box(
-                        modifier = Modifier.padding(all=5.dp)
-                            .fillMaxWidth()
-                            .background(color = Color(0xFFF4F4F4),
-                            shape = RoundedCornerShape(size = 12.dp))
-                            .padding(all = 15.dp)
-                    ) {
-                        Text(modifier = Modifier.align(Alignment.CenterStart),
-                            text = category,
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.DarkGray)
-
-                        IconButton( modifier = Modifier.padding(all=5.dp)
-                            .padding(end = 35.dp)
-                            .size(20.dp)
-                            .align(Alignment.CenterEnd),
-                            onClick = {  },
-                        ) {
-                            Icon(Icons.Filled.Edit, contentDescription = null, tint = Color.Gray)
-                        }
-                        IconButton( modifier = Modifier.padding(all=5.dp)
-                            .size(20.dp)
-                            .align(Alignment.CenterEnd),
-                            onClick = {  },
-                        ) {
-                            Icon(Icons.Filled.Delete, contentDescription = null, tint = Color.Gray)
-                        }
-                    }
-                }
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ){
+               categoryList?.forEach{category ->
+                   CategoryList(category)
+               }
             }
+
+
         }
 
         if (uiState.value.dismissDialogState) {
@@ -125,6 +113,67 @@ fun EditCategory(
         }
     }
 
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun CategoryList(
+    categoryName: String
+    ){
+
+    val swipeableState = rememberSwipeableState(initialValue = 0)
+    val scope = rememberCoroutineScope()
+
+    Box(
+        modifier = Modifier.padding(all=5.dp)
+            .fillMaxWidth()
+            .swipeable(
+                state = swipeableState,
+                anchors = mapOf(
+                    0f to 0,
+                    200f to 1,
+                    200f to 2
+                ),
+                thresholds = { _, _ ->
+                    FractionalThreshold(0.5f)
+                },
+                orientation = Orientation.Horizontal
+            )
+            .background(Color.White)
+    ) {
+        IconButton( modifier = Modifier.padding(all=5.dp)
+            .size(20.dp)
+            .align(Alignment.CenterStart),
+            onClick = {  },
+        ) {
+            Icon(Icons.Filled.Edit, contentDescription = null, tint = Color.Gray)
+        }
+        IconButton( modifier = Modifier.padding(all=5.dp)
+            .padding(start = 35.dp)
+            .size(20.dp)
+            .align(Alignment.CenterStart),
+            onClick = {  },
+        ) {
+            Icon(Icons.Filled.Delete, contentDescription = null, tint = Color.Gray)
+        }
+
+        Box(
+            modifier = Modifier
+                .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
+                .background(color = Color(0xFFF4F4F4),
+                    shape = RoundedCornerShape(size = 12.dp))
+                .fillMaxWidth()
+                .padding(15.dp)
+        ){
+
+                Text(modifier = Modifier,
+                    text = categoryName,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.DarkGray)
+
+        }
+    }
 }
 
 fun selectCategoryList(viewModel: CategoryViewModel): Flow<List<String>?> = flow {
